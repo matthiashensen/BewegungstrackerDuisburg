84b18c23f946f1ce52262b001fcc02e7bb38acd9
lukasgierth <lukas.gierth@mailbox.org>
2016-05-28 16:18:08 +0200

    implement locationmanager, not yet working. TODO: check permission

--- app/src/main/java/org/hsbo/gierthhensen/bewegungstrackerduisburg/StartActivity.java
+++ app/src/main/java/org/hsbo/gierthhensen/bewegungstrackerduisburg/StartActivity.java
@@ -1,8 +1,22 @@
 package org.hsbo.gierthhensen.bewegungstrackerduisburg;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,10 +26,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-
+@SuppressLint({"ShowToast"})
 public class StartActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,26 @@ public class StartActivity extends AppCompatActivity
         setContentView(R.layout.activity_start);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /**
+         * Check network connection
+         */
+        if (!isOnline()) {
+            final AlertDialog.Builder myDialog = new AlertDialog.Builder(StartActivity.this);
+            myDialog.setTitle(R.string.dialogtitle);
+            myDialog.setMessage(R.string.dialogmessage);
+            myDialog.setPositiveButton(R.string.dialogbutton, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivityForResult(new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS), 0);
+                }
+            });
+
+            myDialog.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            myDialog.show();
+        }
 
         /**
          * Floating Button
@@ -108,11 +143,90 @@ public class StartActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-
+            getNetworkPosition();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /**
+     * Checks Network connection
+     */
+    public boolean isOnline() {
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * get Position via Network
+     * TODO: check permission
+     */
+    public void getNetworkPosition() {
+
+        String lat;
+        String lon;
+
+        LocationManager lManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+        boolean isNW = lManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (isNW == false) {
+
+            AlertDialog.Builder myDialog = new AlertDialog.Builder(StartActivity.this);
+
+            myDialog.setTitle("a");
+            myDialog.setMessage("a");
+            myDialog.setPositiveButton("a", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivityForResult(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0);
+                }
+            });
+
+            myDialog.show();
+
+        } else {
+            Location loc;
+            try {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    return;
+                }
+                loc = lManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                lat = Double.toString(loc.getLatitude());
+                lon = Double.toString(loc.getLongitude());
+                String coords = (lon + ","+ lat);
+
+                Toast.makeText(this, coords, Toast.LENGTH_LONG).show();
+
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    /**
+     * Handle LocationListener
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {}
+
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
 }
